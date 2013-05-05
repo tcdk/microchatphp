5013eac60d7691d4186c1812b0a5cf91cb8aca9c
Thomas <tc@mint>
2013-05-05 11:43:10 +0200

    Lots of clean up Mostly changed the httprequest responce go to one place and have that function take care of the response type (one/all)

---
+++ chatlib.php
@@ -0,0 +1,66 @@
+<?php 
+
+if(!@file_exists('config.php') ) {
+  $links = array();  
+} else {
+   include_once('config.php');
+}
+
+function Smilify(&$subject)
+{
+    $smilies = array(
+        ';)'  => 'wink',
+        ';-)' => 'wink',
+        ':p'  => 'razz',
+        ':-p' => 'razz',
+        ':P'  => 'razz',
+        ':-P' => 'razz',
+        ':D'  => 'biggrin',
+        ':-D' => 'biggrin',
+        ':)'  => 'smile',
+        ':-)' => 'smile',
+        ':('  => 'nosmile',
+        ':-(' => 'nosmile',
+        ':*' => 'bussi',
+        '&amp;lt;3' => 'heart'
+        
+    );
+
+    $sizes = array(
+        'biggrin' => 20,
+        'cool' => 20,
+        'haha' => 20,
+        'mellow' => 20,
+        'ohmy' => 20,
+        'sad' => 20,
+        'smile' => 18,
+        'tongue' => 20,
+        'wink' => 20,
+    );
+
+    $replace = array();
+    foreach ($smilies as $smiley => $imgName)
+    {
+        $size = $sizes[$imgName];
+        array_push($replace, 
+        '<img  height="14px" src="imgs/smiley_emoticons_'.$imgName.'.gif" alt="'.$smiley.'"  />');
+    }
+    $subject = str_replace(array_keys($smilies), $replace, $subject);
+}
+
+function buildline($nick, $text)
+{
+	$c = "black";
+	if (isset($usercolors[$nick]))
+	  $c = $usercolors[$nick];
+	
+	$new_date = strtotime(time()) + strtotime("+0 hours");
+
+	$nick = $nick . " (" . date('H:i', $new_date).")";
+	$msg  = isset($_GET['msg']) ? htmlEscapeAndLinkUrls(htmlentities($_GET['msg'], ENT_NOQUOTES, "UTF-8")) : ".";
+	Smilify($msg);
+	$line = "<p><span class=\"name\" style=\"color:".$c."\">".$nick .": </span><span class=\"txt\">$msg</span></p>";
+	return $line;
+}
+
+?>
